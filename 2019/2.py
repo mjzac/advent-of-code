864508e469c5104496b8a961b8f630ef7ca3acde
Melson Zacharias <mzacharias@perleybrook.com>
2019-12-02 20:19:48 +0530

    Day 2 part#2

--- 2019/2.py
+++ 2019/2.py
@@ -1,3 +1,5 @@
+# https://adventofcode.com/2019/day/2
+
 program = None
 
 
@@ -8,28 +10,27 @@ def reset_program():
 
 
 def run():
-    opcode_position = 0
-    operand1_position = 1
-    operand2_position = 2
-    result_position = 3
+    opcode_idx = 0
+    operand1_idx = 1
+    operand2_idx = 2
+    result_idx = 3
 
-    opcode = program[opcode_position]
+    opcode = program[opcode_idx]
+
     while opcode != 99:
         if opcode == 1:
-            program[program[result_position]] = (
-                program[program[operand1_position]]
-                + program[program[operand2_position]]
+            program[program[result_idx]] = (
+                program[program[operand1_idx]] + program[program[operand2_idx]]
             )
         elif opcode == 2:
-            program[program[result_position]] = (
-                program[program[operand1_position]]
-                * program[program[operand2_position]]
+            program[program[result_idx]] = (
+                program[program[operand1_idx]] * program[program[operand2_idx]]
             )
-        opcode_position += 4
-        operand1_position += 4
-        operand2_position += 4
-        result_position += 4
-        opcode = program[opcode_position]
+        opcode_idx += 4
+        operand1_idx += 4
+        operand2_idx += 4
+        result_idx += 4
+        opcode = program[opcode_idx]
     return program[0]
 
 
@@ -37,7 +38,19 @@ def part_1():
     reset_program()
     program[1] = 12
     program[2] = 2
+    return run()
+
+
+def part_2():
+    for noun in range(100):
+        for verb in range(100):
+            reset_program()
+            program[1] = noun
+            program[2] = verb
+            output = run()
+            if output == 19690720:
+                return noun * 100 + verb
 
 
 if __name__ == "__main__":
-    print(part_1)
+    print(part_2())
