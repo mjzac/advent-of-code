4b42fe5db603ced68a0b0615d0697fd91749f14a
Melson Zacharias <jmelson7@gmail.com>
2019-12-05 23:48:35 +0530

    Refactor Day 5 code

--- 2019/5.py
+++ 2019/5.py
@@ -11,133 +11,86 @@ def reset_program():
         program = list(map(int, f.readline().split(",")))
 
 
-def run(is_part2=False):
+def get_mods(instruction):
+    parameter_modes_str = reversed(str(instruction)[:-2])
+    parameter_modes = list(map(int, parameter_modes_str))
+    while len(parameter_modes) < 4:
+        parameter_modes.append(0)
+    return parameter_modes
+
+
+POSITION_MODE = 0
+
+
+def get_operand(idx, mode):
+    if mode == POSITION_MODE:
+        return program[program[idx]]
+    else:
+        return program[idx]
+
+
+def get_two_operands(ptr):
+    modes = get_mods(program[ptr])
+    return (get_operand(ptr + 1, modes[0]), get_operand(ptr + 2, modes[1]))
+
+
+def set_value(idx, val):
+    program[idx] = val
+
+
+def run(input_id=1):
     instruction_ptr = 0
     instruction_ptr_modified = False
     opcode = int(str(program[instruction_ptr])[-2:])
+    output_val = None
     while opcode != 99:
-        parameter_mode = reversed(str(program[instruction_ptr])[:-2])
-        parameter_modes = list(map(int, parameter_mode))
-        while len(parameter_modes) < 4:
-            parameter_modes.append(0)
         if opcode == 1:
-            operand1_idx = program[instruction_ptr + 1]
-            operand2_idx = program[instruction_ptr + 2]
-            result_idx = program[instruction_ptr + 3]
-            operand1 = (
-                program[operand1_idx] if parameter_modes[0] == 0 else operand1_idx
-            )
-
-            operand2 = (
-                program[operand2_idx] if parameter_modes[1] == 0 else operand2_idx
-            )
-            program[result_idx] = operand1 + operand2
+            operand1, operand2 = get_two_operands(instruction_ptr)
+            set_value(program[instruction_ptr + 3], operand1 + operand2)
         elif opcode == 2:
-            operand1_idx = program[instruction_ptr + 1]
-            operand2_idx = program[instruction_ptr + 2]
-            result_idx = program[instruction_ptr + 3]
-            operand1 = (
-                program[operand1_idx] if parameter_modes[0] == 0 else operand1_idx
-            )
-            operand2 = (
-                program[operand2_idx] if parameter_modes[1] == 0 else operand2_idx
-            )
-            program[result_idx] = operand1 * operand2
-
+            operand1, operand2 = get_two_operands(instruction_ptr)
+            set_value(program[instruction_ptr + 3], operand1 * operand2)
         elif opcode == 3:
-            if is_part2:
-                input_id = 5
-            else:
-                input_id = 1
-            program[program[instruction_ptr + 1]] = input_id
+            set_value(program[instruction_ptr + 1], input_id)
         elif opcode == 4:
-            ptr_increment = 2
-            print(program[program[instruction_ptr + 1]])
-        elif is_part2 and opcode == 5:
-            operand1_idx = program[instruction_ptr + 1]
-            if parameter_modes[0] == 0:
-                operand1 = program[operand1_idx]
-            else:
-                operand1 = operand1_idx
-
-            operand2_idx = program[instruction_ptr + 2]
-            if parameter_modes[1] == 0:
-                operand2 = program[operand2_idx]
-            else:
-                operand2 = operand2_idx
+            output_val = program[program[instruction_ptr + 1]]
+        elif opcode == 5:
+            operand1, operand2 = get_two_operands(instruction_ptr)
             if operand1 != 0:
                 instruction_ptr = operand2
                 instruction_ptr_modified = True
 
-        elif is_part2 and opcode == 6:
-            operand1_idx = program[instruction_ptr + 1]
-            if parameter_modes[0] == 0:
-                operand1 = program[operand1_idx]
-            else:
-                operand1 = operand1_idx
-
-            operand2_idx = program[instruction_ptr + 2]
-            if parameter_modes[1] == 0:
-                operand2 = program[operand2_idx]
-            else:
-                operand2 = operand2_idx
+        elif opcode == 6:
+            operand1, operand2 = get_two_operands(instruction_ptr)
             if operand1 == 0:
                 instruction_ptr = operand2
                 instruction_ptr_modified = True
-        elif is_part2 and opcode == 7:
-            operand1_idx = program[instruction_ptr + 1]
-            if parameter_modes[0] == 0:
-                operand1 = program[operand1_idx]
-            else:
-                operand1 = operand1_idx
+        elif opcode == 7:
+            operand1, operand2 = get_two_operands(instruction_ptr)
+            result_val = 1 if operand1 < operand2 else 0
+            set_value(program[instruction_ptr + 3], result_val)
 
-            operand2_idx = program[instruction_ptr + 2]
-            if parameter_modes[1] == 0:
-                operand2 = program[operand2_idx]
-            else:
-                operand2 = operand2_idx
-
-            if operand1 < operand2:
-                result_val = 1
-            else:
-                result_val = 0
-            program[program[instruction_ptr + 3]] = result_val
-
-        elif is_part2 and opcode == 8:
-            operand1_idx = program[instruction_ptr + 1]
-            if parameter_modes[0] == 0:
-                operand1 = program[operand1_idx]
-            else:
-                operand1 = operand1_idx
-
-            operand2_idx = program[instruction_ptr + 2]
-            if parameter_modes[1] == 0:
-                operand2 = program[operand2_idx]
-            else:
-                operand2 = operand2_idx
-
-            if operand1 == operand2:
-                result_val = 1
-            else:
-                result_val = 0
-            program[program[instruction_ptr + 3]] = result_val
+        elif opcode == 8:
+            operand1, operand2 = get_two_operands(instruction_ptr)
+            result_val = 1 if operand1 == operand2 else 0
+            set_value(program[instruction_ptr + 3], result_val)
         if not instruction_ptr_modified:
             instruction_ptr += instruction_lengths[opcode]
         instruction_ptr_modified = False
         opcode = int(str(program[instruction_ptr])[-2:])
 
-    return program[0]
+    return output_val
 
 
 def part_1():
     reset_program()
-    return run()
+    return run(input_id=1)
 
 
 def part_2():
     reset_program()
-    return run(is_part2=True)
+    return run(input_id=5)
 
 
 if __name__ == "__main__":
-    part_2()
+    print(part_2())
